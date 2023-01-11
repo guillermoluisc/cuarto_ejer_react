195d193952f0b66f72f2995427b725446bd569a8
Guillermo Casanova <guillermoluisc@gmail.com>
2023-01-10 21:33:43 -0300

    Resolucion primer cuarto ejercicio (primero react), agregado de compontente personal con nombre y apellido

---
+++ React/counter-app/src/Fisrtcomponent.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Fisrtcomponent() {
+ const nombre="Guillermo Luis"
+ const apellidos="Casanova"
+
+  return (
+    <h1>
+        {nombre}  {apellidos}
+    </h1>
+  )
+}
+
+export default Fisrtcomponent
---
+++ React/counter-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Fisrtcomponent from './Fisrtcomponent'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Fisrtcomponent />
+  </React.StrictMode>,
+)
