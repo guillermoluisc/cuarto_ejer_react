92773b47215d54694700bbf06fe6b1946f0c3948
Guillermo Casanova <guillermoluisc@gmail.com>
2023-03-24 00:12:14 -0300

    Ajustado el test, a la recomendacion de la logica dentro del mismo.

--- React/counter-app/test/demo.test.jsx
+++ React/counter-app/test/demo.test.jsx
@@ -1,5 +1,5 @@
-const mensaje1="Primer String"
-const mensaje2=mensaje1.trim()
 test('comparacion con  mensaje1 y mensaje2', () => {
-    expect(mensaje1).toBe(mensaje2);
-  });
+  const mensaje1 = "Primer String"
+  const mensaje2 = mensaje1.trim()
+  expect(mensaje1).toBe(mensaje2);
+});
